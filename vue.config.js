859d04335fd85c76a0568327993ba13c0d1f8052
Olusunmbola Oluwasijibomi <sijibomiolusunmbola@gmail.com>
2025-03-29 18:01:32 +0100

    feat: install necessary loader plugin (babel-loader), update vue config in preparation for deployment and refine root app font sizes and appearance

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
+const webpack = require("webpack");
 const { defineConfig } = require("@vue/cli-service");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: '/',
+  
   configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()],
+    plugins: [new BundleAnalyzerPlugin() && new webpack.DefinePlugin({
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
+    })],
     stats: "detailed",
+  },
+
+  devServer: {
+    compress: true,
+    port: 8080,
+    open: true,
+    hot: true,
+    watchFiles: ['src/**/*', 'public/**/*'],
   },
 });
